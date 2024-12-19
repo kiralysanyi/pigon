69ddce177e0653db74482f3720973d7c682d56bd
Király Sándor <kiralysandor1986@gmail.com>
2024-12-20 00:30:35 +0100

    - Added video to calls - Added screenshare to calls

--- server/public/webui/js/callUI.js
+++ server/public/webui/js/callUI.js
@@ -20,6 +20,9 @@ const callid = location.hash.substring(1);
 
 // TODO: host our own peerjs server and add it's config here
 const peer = new Peer(deviceID, { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }], 'sdpSemantics': 'unified-plan' });
+const vpeer = new Peer("video" + deviceID, { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }], 'sdpSemantics': 'unified-plan' });
+const speer = new Peer("screen" + deviceID, { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }], 'sdpSemantics': 'unified-plan' });
+
 
 await fetch("/api/v1/chat/registerPeer", {
     method: "POST",
@@ -67,6 +70,23 @@ async function getMicrophoneStream() {
     } catch (error) {
         console.error('Error accessing microphone:', error);
         throw new Error('Microphone permission denied or not available');
+    }
+}
+
+/**
+ * Requests camera access and returns a MediaStream.
+ * @returns {Promise<MediaStream>} A promise that resolves to the MediaStream.
+ * @throws {Error} If the user denies permission or an error occurs.
+ */
+async function getCameraStream() {
+    try {
+        // Request camera access
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        console.log('Camera access granted');
+        return stream;
+    } catch (error) {
+        console.error('Error accessing camera:', error);
+        throw new Error('Camera permission denied or not available');
     }
 }
 
@@ -158,3 +178,208 @@ function startTimer() {
 }
 
 startTimer();
+
+/**
+ * Stop all tracks in a MediaStream.
+ * @param {MediaStream} mediaStream - The MediaStream to stop.
+ */
+function stopMediaStream(mediaStream) {
+    if (!mediaStream) {
+        throw new Error("No MediaStream provided.");
+    }
+
+    mediaStream.getTracks().forEach((track) => track.stop());
+}
+
+//video streaming things
+
+//handle incoming video stream
+let videoStreamCount = 0;
+
+vpeer.on("call", (mediaConnection) => {
+    mediaConnection.on("stream", (stream) => {
+        videoStreamCount++;
+        let videoElement = document.createElement("video");
+        videoElement.muted = true;
+        videoElement.autoplay = true;
+        videoElement.srcObject = stream;
+        document.getElementById("videocontainer").appendChild(videoElement);
+        document.getElementById("videocontainer").style.display = "block";
+
+        mediaConnection.on("close", () => {
+            videoStreamCount--;
+            videoElement.remove();
+            if (videoStreamCount == 0) {
+                document.getElementById("videocontainer").style.display = "none";
+            }
+        })
+    })
+
+    mediaConnection.answer(new MediaStream())
+})
+
+/**
+ * Returns a function to stop the video streaming
+ */
+let startVideoStream = async () => {
+    let selfView = document.createElement("video");
+    selfView.classList.add("selfView");
+    selfView.autoplay = true;
+    selfView.muted = true;
+    let stream = await getCameraStream();
+    selfView.srcObject = stream;
+    document.body.appendChild(selfView);
+    let connections = [];
+    for (let i in peers) {
+        let mediaConnection = vpeer.call("video" + peers[i], stream);
+        connections.push(mediaConnection);
+    }
+
+    return () => {
+        for (let i in connections) {
+            connections[i].close();
+            stopMediaStream(stream);
+            selfView.remove();
+
+        }
+    }
+}
+
+
+let stopVideoStream = null;
+let toggleVideo = async () => {
+    if (stopVideoStream == null) {
+        document.getElementById("screenbtn").style.display = "none";
+        document.getElementById("videobtn").innerHTML = '<i class="fa-solid fa-video"></i>';
+        stopVideoStream = await startVideoStream();
+        return;
+    }
+
+    stopVideoStream();
+    document.getElementById("screenbtn").style.display = "block";
+    document.getElementById("videobtn").innerHTML = '<i class="fa-solid fa-video-slash"></i>';
+    stopVideoStream = null;
+}
+
+
+
+document.getElementById("videobtn").addEventListener("click", () => {
+    toggleVideo();
+})
+
+//screen sharing things
+
+/**
+ * Check if screen capture is supported.
+ * @returns {boolean} - True if screen capture is supported, false otherwise.
+ */
+function canCaptureScreen() {
+    return !!navigator.mediaDevices?.getDisplayMedia;
+}
+
+/**
+ * Capture the screen and return the MediaStream.
+ * @returns {Promise<MediaStream>} - A promise that resolves with the screen MediaStream.
+ * @throws {Error} - If screen capture is not supported or the user denies the request.
+ */
+async function captureScreen() {
+    if (!canCaptureScreen()) {
+        throw new Error("Screen capture is not supported in this browser.");
+    }
+
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia({
+            video: true,
+        });
+        return mediaStream;
+    } catch (error) {
+        throw new Error(`Failed to capture screen: ${error.message}`);
+    }
+}
+
+if (canCaptureScreen() == true) {
+    document.getElementById("screenbtn").style.display = "block";
+}
+
+
+//handle incoming video stream
+let svideoStreamCount = 0;
+
+speer.on("call", (mediaConnection) => {
+    mediaConnection.on("stream", (stream) => {
+        svideoStreamCount++;
+        let videoElement = document.createElement("video");
+        videoElement.muted = false;
+        videoElement.autoplay = true;
+        videoElement.srcObject = stream;
+        document.getElementById("videocontainer").appendChild(videoElement);
+        document.getElementById("videocontainer").style.display = "block";
+
+        mediaConnection.on("close", () => {
+            svideoStreamCount--;
+            videoElement.remove();
+            if (svideoStreamCount == 0) {
+                document.getElementById("videocontainer").style.display = "none";
+            }
+        })
+    })
+
+    mediaConnection.answer(new MediaStream())
+})
+
+/**
+ * Returns a function to stop the video streaming
+ */
+let startsVideoStream = async () => {
+    let selfView = document.createElement("video");
+    selfView.classList.add("selfView");
+    selfView.autoplay = true;
+    selfView.muted = true;
+    let stream = await captureScreen();
+    selfView.srcObject = stream;
+    document.body.appendChild(selfView);
+
+    let connections = [];
+    for (let i in peers) {
+        let mediaConnection = speer.call("video" + peers[i], stream);
+        connections.push(mediaConnection);
+    }
+
+    return () => {
+        for (let i in connections) {
+            connections[i].close();
+            stopMediaStream(stream);
+            selfView.remove();
+        }
+    }
+}
+
+
+let stopsVideoStream = null;
+let togglesVideo = async () => {
+    if (stopsVideoStream == null) {
+        document.getElementById("videobtn").style.display = "none";
+        document.getElementById("screenbtn").style.backgroundColor = "red";
+        stopsVideoStream = await startsVideoStream();
+        return;
+    }
+
+    stopsVideoStream();
+    document.getElementById("videobtn").style.display = "block";
+    document.getElementById("screenbtn").style.backgroundColor = "transparent";
+    stopsVideoStream = null;
+}
+
+document.getElementById("screenbtn").addEventListener("click", () => {
+    togglesVideo();
+})
+
+
+
+document.getElementById("closebtn").style.display = "block";
+
+try {
+    await navigator.wakeLock.request("screen");
+} catch (err) {
+    console.error(`Failed to acquire wake lock: ${err.message}`);
+}
